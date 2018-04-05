0d9d45196eeaf7b0fa8f7a56382391b6ae55872e
gcmillar <gcmillar@ncsu.edu>
2018-04-05 01:46:33 -0400

    pls

--- publish.sh
+++ publish.sh
@@ -38,10 +38,6 @@ fi
 echo "Building..."
 ./build.sh
 
-# build also the lectures to avoid any possible conflicts
-cd lectures
-./build.sh
-cd ..
 
 if [ $? -ne 0 ]; then
     echo "Build failed. Use ./build.sh script to debug the issue." 1>&2;
